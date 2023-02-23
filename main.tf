69875b23a27b8dd9312afe924f0cf34a16c2e648
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-02-23 13:38:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@
 module "rg_name" {
   # Terraform Cloud PMR use
   source  = "app.terraform.io/embergertf/base/azurerm"
-  version = "~>2.0.0"
+  version = "~>2.0.1"
 
   name_override = var.name_override
 
