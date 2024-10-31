a9fcb75c06ab16bbaa4ae11ac0a142ca06d52d95
Emmanuel <bergerat62@gmail.com>
2024-10-31 13:28:25 -0400

    Upgraded versions

--- Example/example_main.tf
+++ Example/example_main.tf
@@ -1,11 +1,11 @@
 #
-# Copyright 2023 - Emmanuel Bergerat
+# Copyright 2024 - Emmanuel Bergerat
 #
 
 #--------------------------------------------------------------
 #   Test Resource Group module Main
 #--------------------------------------------------------------
-module "rg1_region1" {
+module "local_rg_region1" {
   # Local use
   source = "../../terraform-azurerm-resourcegroup"
 
@@ -24,10 +24,10 @@ module "rg1_region1" {
 
   additional_tags = var.additional_tags
 }
-module "rg2_region2" {
+module "tfc_rg_region2" {
   # Terraform Cloud use
   source  = "app.terraform.io/embergertf/resourcegroup/azurerm"
-  version = "~>1.3.3"
+  version = "~> 1.3"
 
   # name_override = var.name_override
 
@@ -44,14 +44,13 @@ module "rg2_region2" {
 
   additional_tags = var.additional_tags
 }
-
-module "rg3_region1" {
+module "tfc_rg_override" {
   # Terraform Cloud PMR use
   source  = "app.terraform.io/embergertf/resourcegroup/azurerm"
-  version = "~>1.3.3"
+  version = "~> 1.3"
 
   name_override = "override-test-embergertf"
-  region_code     = var.region_code1
+  region_code   = var.region_code1
 
   # subsc_code      = var.subsc_code
   # env             = var.env
